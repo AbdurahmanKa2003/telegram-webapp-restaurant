a75ebc8c6982a2ed7fe91fcb166e7864db2c0c29
AbdurahmanKa2003 <147339917+AbdurahmanKa2003@users.noreply.github.com>
2025-12-28 14:17:52 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+# Замените 'webapp_view' на реальное название вашей функции из core.views
+from core.views import webapp_view 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # Добавьте эту строку ниже:
+    path("webapp/", webapp_view, name="webapp"), 
     path("", include("core.urls")),
     path("api/", include("orders.api_urls")),
     path("api/catalog/", include("catalog.api_urls")),
 ]
 
-# Всегда добавляем эти пути, чтобы Django знал, где искать файлы в DEBUG и после collectstatic
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
