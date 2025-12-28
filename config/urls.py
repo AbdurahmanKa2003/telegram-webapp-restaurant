02f63ca9f61b99d0614a9e4a842c4d015be8f230
AbdurahmanKa2003 <147339917+AbdurahmanKa2003@users.noreply.github.com>
2025-12-28 07:18:01 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,15 @@
 from django.contrib import admin
-from django.urls import path
-from core.views import webapp
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("webapp/", webapp, name="webapp"),
     path("", include("core.urls")),
     path("api/", include("orders.api_urls")),
     path("api/catalog/", include("catalog.api_urls")),
 ]
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+
+# Всегда добавляем эти пути, чтобы Django знал, где искать файлы в DEBUG и после collectstatic
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
