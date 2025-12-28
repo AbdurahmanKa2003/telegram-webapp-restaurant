ee48dadbed3d08a9746ef5a8a5728b1ec5bee5b3
AbdurahmanKa2003 <147339917+AbdurahmanKa2003@users.noreply.github.com>
2025-12-28 15:16:22 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,17 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# Исправленный импорт: импортируем именно 'webapp'
 from core.views import webapp 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # Исправленный путь: используем функцию 'webapp'
     path("webapp/", webapp, name="webapp"), 
     path("", include("core.urls")),
     path("api/", include("orders.api_urls")),
     path("api/catalog/", include("catalog.api_urls")),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Эти строки нужны ТОЛЬКО для локальной разработки. 
+# На Render WhiteNoise сам подхватит STATIC_ROOT.
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
